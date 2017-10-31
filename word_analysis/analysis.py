020257144c769d0e8f98b6268c891857b2663d40
hansminner <360312085@qq.com>
2017-10-31 12:03:28 +0800

    还有需要增加的地方 统计词频,阅读全文

--- word_analysis/analysis.py
+++ word_analysis/analysis.py
@@ -3,13 +3,28 @@ from collections import Counter  # 统计词频
 from wordcloud import WordCloud, STOPWORDS  # 生成词云、通用词
 import matplotlib.pyplot as plt  # 在线显示
 import jieba
+from jieba import analyse
 
 # from word_analysis import word_counter
 
 # http://blog.csdn.net/vivian_ll/article/details/68067574 成功地址
 # http://www.jianshu.com/p/c612847ffa72 要看的地址
-comment_text = open('output.txt', 'r', encoding='utf-8').read()
-cut_text = " ".join(jieba.cut(comment_text))
+
+"""
+a=jieba.analyse.extract_tags(sentence, topK = 20, withWeight = False, allowPOS = ())
+# sentence:待提取的文本。# topK:返回几个 TF/IDF 权重最大的关键词，默认值为20。
+# withWeight:是否一并返回关键词权重值，默认值为False。
+# allowPOS:仅包括指定词性的词，默认值为空，即不进行筛选。
+"""
+comment_text = jieba.cut(open('output.txt', 'r', encoding='utf-8').read(), cut_all=False)
+# 必须使用join两个字符连接直接传会报错
+comment_text_1 = "".join(comment_text)
+# 通用停用词
+jieba.analyse.set_stop_words('stopword.txt')
+# 自己项目的停用词
+jieba.analyse.set_stop_words('self_stopword.txt')
+tags = jieba.analyse.extract_tags(comment_text_1, 50)
+cut_text = " ".join(tags)
 cloud = WordCloud(
     # 设置字体，不指定就会出现乱码
     font_path="HYQiHei-25J.ttf",
@@ -29,7 +44,7 @@ cloud = WordCloud(
 # wc = Counter(data)  # 基于Counter自定义的子类（留作业：结巴分词、停用词）
 # wordcloud = WordCloud(background_color="white", width=600, height=400, margin=5).fit_words(dict_fre)
 """
-wordcloud = WordCloud(background_color="white", width=600, height=400, margin=5).generate(cut_text)
+wordcloud = WordCloud(background_color="white", width=5000, height=5000, margin=5).generate(cut_text)
 plt.imshow(wordcloud)
 plt.axis("off")
 plt.show()
